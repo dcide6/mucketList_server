0356b7f2470d7f2b33e776ffd50adb947dbf345f
dcide6 <dcide6@gmail.com>
2021-05-16 23:05:59 +0900

    project init commit

---
+++ src/main/java/com/siksaurus/mucketlist_server/MucketListServerApplication.java
@@ -0,0 +1,13 @@
+package com.siksaurus.mucketlist_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucketListServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MucketListServerApplication.class, args);
+    }
+
+}
